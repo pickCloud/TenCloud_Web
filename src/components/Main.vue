99a11ea46b87bb32c1628d8728717a1775a25829
Hainc <cnria@qq.com>
2017-04-28 09:17:48 +0800

    字体库 位置修正！主机详情页下边距修复

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,5 +1,14 @@
 <template>
   <div class="v-main">
-    this is main
+    <i class="ten-icon">&#xe606;</i>
+    <i class="ten-icon">&#xe734;</i>
+    <i class="ten-icon">&#xe669;</i>
+    <i class="ten-icon">&#xe685;</i>
   </div>
 </template>
+
+<style scoped lang="scss">
+  .ten-icon {
+    font-size: 70px;
+  }
+</style>
