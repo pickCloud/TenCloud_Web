719167bb820edac832cbca521cf3f839cbebcafd
Hainc <cnria@qq.com>
2017-05-24 18:19:19 +0800

    api地址判断

--- src/main.js
+++ src/main.js
@@ -11,9 +11,10 @@ Vue.prototype.$http = axios
 Vue.config.productionTip = false
 Vue.use(Tenui)
 // Vue.prototype.$G = Global
+const islocal = /.+localhost.+/.test(window.location.href)
 Vue.prototype.$API = {
-  ws: 'ws://192.168.1.116:8010',
-  api: 'http://192.168.1.116'
+  ws: islocal ? 'ws://192.168.1.116:8010' : 'ws://47.94.18.22',
+  api: islocal ? 'http://192.168.1.116' : 'http://47.94.18.22'
 }
 Vue.prototype.$COMMON = {}
 
