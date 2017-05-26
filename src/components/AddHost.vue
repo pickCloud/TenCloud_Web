1093ce7320e4e0a0a2f23a789b7e3562f659c885
Hainc <cnria@qq.com>
2017-05-26 15:28:18 +0800

    Fix bugs in AddHost

--- src/components/AddHost.vue
+++ src/components/AddHost.vue
@@ -15,23 +15,37 @@
             <!--<li class="switch-item">ubuntu</li>-->
             <!--<li class="switch-item">ubuntu</li>-->
           <!--</ul>-->
-          <div class="margin-b-16 margin-t-16">选择集群</div>
-          <ul class="switch-box">
+          <div class="margin-b-16">主机信息</div>
+          <div class="row no-gutter add-host_info">
+            <div class="col s4 m2 l1 center-align add-host_label">主机名称</div>
+            <div class="col s8 m10 l11"><input type="text" class="form-control" v-model="formdata.name"></div>
+
+            <div class="col s4 m2 l1 center-align add-host_label">IP</div>
+            <div class="col s8 m10 l11"><input type="text" class="form-control" v-model="formdata.ip"></div>
+
+            <div class="col s4 m2 l1 center-align add-host_label">用户名</div>
+            <div class="col s8 m10 l11"><input type="text" class="form-control" v-model="formdata.username"></div>
+
+            <div class="col s4 m2 l1 center-align add-host_label">密码</div>
+            <div class="col s8 m10 l11"><input type="text" class="form-control" v-model="formdata.passwd"></div>
+          </div>
+
+          <div class="margin-b-16" v-if="clusters.length > 0">选择集群</div>
+          <ul class="switch-box" v-if="clusters.length > 0">
             <li class="switch-item" :class="{'active': item.id === selectid}" v-for="item in clusters" @click="selectCluster(item.id)">{{item.name}}</li>
           </ul>
           <div class="margin-b-16">安装主机监控程序</div>
           <div class="tcm-listener">curl -sSL http://192.168.1.116/supermonitor/master.sh | sh -s {{token}}</div>
-          <div class="margin-b-16">主机名称</div>
-          <input type="text" class="form-control" v-model="tcmname" placeholder="TCM_00124">
+
           <div class="save-box" v-if="isok">
             <div class="comb-btn waves-effect lvse" @click="addHost">保存</div>
           </div>
-          <div class="waiting-link margin-t-16" v-else="">
+          <div class="waiting-link" v-else="">
             <img src="../assets/spin.gif" class="vam" alt=""> <span class="vam">等待连接</span>
           </div>
         </div>
         <div class="tab-cont_item">
-          <div class="margin-b-16 margin-t-16">合作服务商</div>
+          <div class="margin-b-16">合作服务商</div>
           <ul class="switch-box">
             <li class="switch-item switch-item_img"><a href="#"><img src="../assets/aliyun.jpg" alt=""></a></li>
             <li class="switch-item switch-item_img"><a href="#"><img src="../assets/txyun.jpg" alt=""></a></li>
